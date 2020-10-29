6e6db2d1927caef818009ed80ef5065efca40db0
Justin Lane <atari2600@jigglesoft.co.uk>
2020-10-29 23:49:37 +0000

    Application HTML/CSS/JS GUI layout and common JS is work in progress and checked in for backup.

---
+++ www/atari-2600-tool-common.js
@@ -0,0 +1,30 @@
+
+PAL_TV = 0;
+
+NTSC_TV = 1;
+
+TV_SECAM = 2;
+
+
+//colour table pal
+
+//colour table ntsc
+
+//colour table secam
+
+
+
+function getRGBForAtariColour(c, tv)
+{
+}
+
+
+function getAtariColourForRGB(r, g, b, tv)
+{
+}
+
+
+function getBAW()
+{
+}
+
